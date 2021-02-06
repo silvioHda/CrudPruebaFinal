cb4af80add27c862b4d82ed079553508de8307a0
silvio <silvioignacio_01@hotmail.com>
2021-02-06 03:26:01 -0600

    Prueba Finalizada

--- hbs/helpers.js
+++ hbs/helpers.js
@@ -1,16 +1,91 @@
 const hbs = require('hbs');
-
+let json = require('../db.json');
 //helpers
 
-hbs.registerHelper('getAnio',()=>{
-    return new Date().getFullYear();
+
+hbs.registerHelper('available', function(id) {
+    let valor = "";
+    let bool;
+    for (let index = 0; index < json.products.length; index++) {
+        if (json.products[index].id === id) {
+            bool = json.products[index].available;
+        }
+
+    }
+
+
+    if (bool === true) {
+        valor = "Disponible";
+
+    } else {
+        valor = "No disponible";
+    }
+
+    return valor;
 });
 
-hbs.registerHelper('capitalizar',(texto)=>{
-    let palabras = texto.split(' ');
-    palabras.forEach( (palabra,idx)=>{
-        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+hbs.registerHelper('best_seller', function(id) {
+    let valor = "";
+    let bool;
+    for (let index = 0; index < json.products.length; index++) {
+        if (json.products[index].id === id) {
+            bool = json.products[index].best_seller;
+        }
 
-    });
-    return palabras.join(' ');
+    }
+
+
+    if (bool === true) {
+        valor = "Mas vendido";
+
+    } else {
+        valor = "Menos vendido";
+    }
+
+    return valor;
+});
+
+hbs.registerHelper('categories', function(id) {
+    let valor = "";
+    let bool;
+    let category;
+    for (let index = 0; index < json.products.length; index++) {
+        if (json.products[index].id === id) {
+            bool = json.products[index].categories;
+        }
+
+    }
+    if (bool.includes(1)) {
+        category = "drinks"
+    }
+    if (bool.includes(2)) {
+        category = "lunch"
+    }
+    if (bool.includes(3)) {
+        category = "food"
+    }
+    if (bool.includes(4)) {
+        category = "sea"
+    }
+    if (bool.includes(1) && bool.includes(2)) {
+        category = "drinks, lunch"
+    }
+    if (bool.includes(1) && bool.includes(3)) {
+        category = "drinks, food"
+    }
+    if (bool.includes(1) && bool.includes(4)) {
+        category = "drinks, sea"
+    }
+    if (bool.includes(2) && bool.includes(3)) {
+        category = "lunch, food"
+    }
+    if (bool.includes(2) && bool.includes(4)) {
+        category = "lunch, sea"
+    }
+
+    // console.log(bool);
+
+
+
+    return category;
 });
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 const express = require('express')
 const app = express();
 const hbs = require('hbs');
+var getJSON = require('get-json')
+let request = require('request');
 
 require('./hbs/helpers');
 
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
 //Express HBS Engine
-hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+let json = require('./db.json');
 
 
-app.get('/', function (req, res) {
-res.render('home',{
-    nombre: 'silvio',
-    
+
+
+
+app.get('/', function(req, res) {
+    res.render('home', json);
 });
-});
 
-app.get('/about',function(req,res){
+app.get('/about', function(req, res) {
     res.render('about');
 })
 
 
- 
-app.listen(3000,()=>{
+
+
+app.listen(3000, () => {
     console.log('escuchando peticiones en el puerto 3000');
 });
